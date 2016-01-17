d17902e45c06635b56cbb39e2ada3e6968d59608
omarr2012 <omarr2012@tamu.edu>
2016-01-17 17:48:40 -0600

    Added buttons and counting logic
    -image buttons write the drink it corresponds to, to a cvs file
    -every a button is pressed the counter reads the csv file and displays
    how many items there are.
    -The refresh button is suppose to delete the file to essentially reset
    the count to zero(But it isn’t working :().

---
+++ app/src/main/java/yupp/hmbr/jabba/hldmybur/DrinkTracker.java
@@ -0,0 +1,162 @@
+package yupp.hmbr.jabba.hldmybur;
+
+import android.content.Context;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * Created by jabba on 1/16/16.
+ */
+public class DrinkTracker
+{
+    Context mContext = null;
+    String drinksFile = "Drinks.csv";
+
+    //Constructor gives us context
+    public DrinkTracker(Context _context)
+    {
+        mContext = _context;
+    }
+
+    public int parseCsvString(String drinks)
+    {
+
+        return drinks.isEmpty() ? 0 : drinks.split(",").length;
+    }
+
+    public int getDrinkTotal(String drinkType)
+    {
+        int total = -1;
+        switch (drinkType.toLowerCase())
+        {
+            case "total":
+                total = parseCsvString(readFromStorage(drinksFile));
+                break;
+            default:
+                break;
+        }
+        return total;
+    }
+
+    public boolean clearDrinkTotal(String drinkType)
+    {
+        switch (drinkType.toLowerCase())
+        {
+            case "all":
+                File file = new File(mContext.getFilesDir().getAbsolutePath().toString()+drinksFile);
+                return file.delete();
+            default:
+                break;
+        }
+        return false;
+    }
+    //param[0] == filename
+    //param[1] == data
+    public String writeToStorage(String... params)
+    {
+        FileOutputStream fos = null;
+        File file = null;
+        String fileName = params.length > 0 ? params[0] : "";
+        byte[] bString = params.length > 1 ? params[1].getBytes() : new byte[1];
+
+        try
+        {
+            file = mContext.getFilesDir();
+            fos = mContext.openFileOutput(fileName, mContext.MODE_APPEND);
+            fos.write(bString);
+        }
+        catch (Exception ex)
+        {
+            ex.printStackTrace();
+        }
+        finally
+        {
+            try
+            {
+                fos.close();
+            } catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+
+        return file.getPath().toString();
+    }
+
+    public String readFromStorage(String fileName)
+    {
+        int read;
+        String result = "";
+        FileInputStream fis = null;
+        StringBuffer sb = new StringBuffer();
+        File file = new File(mContext.getFilesDir().getPath()+fileName);
+
+        try
+        {
+            if (!file.exists())
+            {
+                mContext.openFileOutput(fileName, mContext.MODE_APPEND);
+            }
+
+            fis = mContext.openFileInput(fileName);
+
+            while ((read = fis.read()) != -1)
+            {
+                sb.append((char)read);
+            }
+
+            result = sb.toString();
+        }
+        catch (Exception ex)
+        {
+            ex.printStackTrace();
+        }
+        finally
+        {
+            try
+            {
+                //Check if ever opened. Happens when file doesn't exist.
+                if (fis != null)
+                {
+                    fis.close();
+                }
+            } catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+
+        return result;
+    }
+
+    public void clearFile(String filename)
+    {
+        FileOutputStream fos = null;
+        File file = null;
+
+        try
+        {
+            file = mContext.getFilesDir();
+            fos = mContext.openFileOutput(filename, mContext.MODE_APPEND);
+
+        }
+        catch (Exception ex)
+        {
+            ex.printStackTrace();
+        }
+        finally
+        {
+            try
+            {
+                fos.close();
+            } catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+}
--- app/src/main/java/yupp/hmbr/jabba/hldmybur/MainActivity.java
+++ app/src/main/java/yupp/hmbr/jabba/hldmybur/MainActivity.java
@@ -1,40 +1,84 @@
 package yupp.hmbr.jabba.hldmybur;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-    private ImageButton imgBtnWine, imgBtnBeer, imgBtnMixed;
+
+public class MainActivity extends AppCompatActivity
+{
+    ImageButton imgBtnWine, imgBtnBeer, imgBtnMixed, imgBtnReset;
+    TextView txtViewTotalDrinks;
+    DrinkTracker dt;
+    String drinksFile = "Drinks.csv";
+    final String TOTAL = "total";
+
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        dt = new DrinkTracker(getApplicationContext());
 
         imgBtnBeer = (ImageButton) findViewById(R.id.imgBtnBeer);
         imgBtnWine = (ImageButton) findViewById(R.id.imgBtnWine);
         imgBtnMixed = (ImageButton) findViewById(R.id.imgBtnMixed);
+        imgBtnReset = (ImageButton) findViewById(R.id.imgBtnReset);
 
-        imgBtnBeer.setOnClickListener(new View.OnClickListener() {
+        txtViewTotalDrinks = (TextView) findViewById(R.id.txtViewTotalCount);
+
+        txtViewTotalDrinks.clearComposingText();
+        txtViewTotalDrinks.setText(Integer.toString(dt.getDrinkTotal(TOTAL)));
+
+        imgBtnReset.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),"BEER!", Toast.LENGTH_SHORT).show();
+            public void onClick(View v)
+            {
+                Toast.makeText(getApplicationContext(), "Reset drink count", Toast.LENGTH_SHORT);
+                dt.clearDrinkTotal("all");
+                txtViewTotalDrinks.clearComposingText();
+                txtViewTotalDrinks.setText(Integer.toString(dt.getDrinkTotal(TOTAL)));
             }
         });
 
-        imgBtnWine.setOnClickListener(new View.OnClickListener() {
+        imgBtnBeer.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "WINE!", Toast.LENGTH_SHORT).show();
+            public void onClick(View v)
+            {
+                dt.writeToStorage(drinksFile, "Beer,");
+                Toast.makeText(getApplicationContext(),"Beer!", Toast.LENGTH_SHORT).show();
+                txtViewTotalDrinks.clearComposingText();
+                txtViewTotalDrinks.setText(Integer.toString(dt.getDrinkTotal(TOTAL)));
             }
         });
 
-        imgBtnMixed.setOnClickListener(new View.OnClickListener() {
+        imgBtnWine.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "Mixed 2 Drink!", Toast.LENGTH_SHORT).show();
+            public void onClick(View v)
+            {
+                dt.writeToStorage(drinksFile, "Wine,");
+                Toast.makeText(getApplicationContext(), "Meh Wine!", Toast.LENGTH_SHORT).show();
+                txtViewTotalDrinks.clearComposingText();
+                txtViewTotalDrinks.setText(Integer.toString(dt.getDrinkTotal(TOTAL)));
+            }
+        });
+
+        imgBtnMixed.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                dt.writeToStorage(drinksFile, "Mix Drink,");
+                Toast.makeText(getApplicationContext(), "It better be whiskey!", Toast.LENGTH_SHORT).show();
+                txtViewTotalDrinks.clearComposingText();
+                txtViewTotalDrinks.setText(Integer.toString(dt.getDrinkTotal(TOTAL)));
             }
         });
     }
