70e18e8b5c831c1ddc8dfa5b03b13327bcf1b63b
omarr2012 <omarr2012@tamu.edu>
2016-01-16 18:44:35 -0600

    Image buttons showing toast
    -made app theme colors darker

--- app/src/main/java/yupp/hmbr/jabba/hldmybur/MainActivity.java
+++ app/src/main/java/yupp/hmbr/jabba/hldmybur/MainActivity.java
@@ -2,12 +2,40 @@ package yupp.hmbr.jabba.hldmybur;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ImageButton imgBtnWine, imgBtnBeer, imgBtnMixed;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imgBtnBeer = (ImageButton) findViewById(R.id.imgBtnBeer);
+        imgBtnWine = (ImageButton) findViewById(R.id.imgBtnWine);
+        imgBtnMixed = (ImageButton) findViewById(R.id.imgBtnMixed);
+
+        imgBtnBeer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"BEER!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        imgBtnWine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "WINE!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        imgBtnMixed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Mixed 2 Drink!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
